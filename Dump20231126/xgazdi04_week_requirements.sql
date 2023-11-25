5170b0241fcf682695c5155e39e9df725716bd60
Peter Kováč <pojzinko8@gmail.com>
2023-11-26 00:08:56 +0100

    Add files via upload

---
+++ Dump20231126/xgazdi04_week_requirements.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: xgazdi04
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `week_requirements`
+--
+
+DROP TABLE IF EXISTS `week_requirements`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `week_requirements` (
+  `activity_request_id` int NOT NULL,
+  `monday_start` int DEFAULT NULL,
+  `monday_end` int DEFAULT NULL,
+  `tuesday_start` int DEFAULT NULL,
+  `tuesday_end` int DEFAULT NULL,
+  `wednesday_start` int DEFAULT NULL,
+  `wednesday_end` int DEFAULT NULL,
+  `thursday_start` int DEFAULT NULL,
+  `thursday_end` int DEFAULT NULL,
+  `friday_start` int DEFAULT NULL,
+  `friday_end` int DEFAULT NULL,
+  `comment` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`activity_request_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `week_requirements`
+--
+
+LOCK TABLES `week_requirements` WRITE;
+/*!40000 ALTER TABLE `week_requirements` DISABLE KEYS */;
+INSERT INTO `week_requirements` VALUES (4,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(5,11,17,9,15,10,NULL,11,16,10,17,'123'),(6,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(7,NULL,NULL,NULL,NULL,NULL,NULL,10,12,NULL,NULL,'ljkhsdakljfhgasdkjlhfalsjkdfhlasjkdf');
+/*!40000 ALTER TABLE `week_requirements` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-26  0:05:09
